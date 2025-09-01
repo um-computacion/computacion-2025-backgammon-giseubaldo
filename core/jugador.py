5110a09ce99dc42ed2a68104d397211a397a5b3a
Gisela Ubaldo <g.ubaldo@alumno.um.edu.ar>
2025-09-01 12:18:42 -0300

    Modificaciones clase dado

--- core/jugador.py
+++ core/jugador.py
@@ -1,4 +1,14 @@
 
+
 class Jugador:
-    def __init__(self):
-        self.nombre = ""
+    """Representa a un jugador."""
+
+    def __init__(self, nombre, color):
+        """Inicializa el jugador.
+        Args:
+            nombre (str): Nombre del jugador.
+            color (str): Color de sus fichas.
+        """
+        self.__nombre__ = nombre
+        self.__color__ = color
+        self.__fichas__ = 15
